51667d5d9d0b08a1a185a69cc91780297d55d8ff
Dmitry Kaukov <dkaukov@opentable.com>
2021-05-23 15:30:05 +1000

    Update debug.h

--- src/debug.h
+++ src/debug.h
@@ -26,7 +26,7 @@ uint8_t __dbg_buff_ptr = 0;
 #include "config.h"
 #include <Syslog.h>
 
-#if defined(NTP_SERVER)
+#if defined(SYSLOG_SERVER)
 
 WiFiUDP udpClient;
 Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, DEVICE_HOSTNAME, APP_NAME, LOG_KERN, SYSLOG_PROTO_BSD);
